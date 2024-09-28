13ae9a74056a0c0a618bfd666f4b7bddbd7d545c
sniffjoke <sniffjoke@gmail.com>
2024-09-29 01:06:31 +0200

    add validation-pipe for create-user

--- src/features/users/api/models/input/create-user.input.model.ts
+++ src/features/users/api/models/input/create-user.input.model.ts
@@ -1,9 +1,15 @@
-import { IsString } from "class-validator";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class UserCreateModel {
     @IsString({message: 'Должно быть строкой'})
+    @Length(3, 10, {message: 'Количество знаков: 3-10'})
     login: string;
+
+    @IsEmail({}, {message: 'Е-майл должен быть валидным'})
     email: string;
+
+    @IsString({message: 'Должно быть строкой'})
+    @Length(6, 20, {message: 'Количество знаков: 6-20'})
     password: string;
 }
 
--- src/features/users/api/users.controller.ts
+++ src/features/users/api/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Query, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Query, UseGuards, UsePipes } from "@nestjs/common";
 import {UsersService} from "../application/users.service";
 import {UsersQueryRepository} from "../infrastructure/users.query-repository";
 import {UserCreateModel} from "./models/input/create-user.input.model";
 import {UserViewModel} from "./models/output/user.view.model";
 import {PaginationBaseModel} from "../../../infrastructure/base/pagination.base.model";
 import { BasicAuthGuard } from "../../../infrastructure/guards/basic-auth.guard";
+import { ValidationPipe } from "../../../infrastructure/pipes/validation.pipe";
 
 @Controller('users')
 export class UsersController {
@@ -14,12 +15,13 @@ export class UsersController {
     ) {}
 
     @Get()
-    @UseGuards(BasicAuthGuard)
+    // @UseGuards(BasicAuthGuard)
     async getAllUsers(@Query() query: any): Promise<PaginationBaseModel<UserViewModel>> {
         const users = await this.usersQueryRepository.getAllUsersWithQuery(query)
         return users
     }
 
+    @UsePipes(ValidationPipe)
     @Post()
     @UseGuards(BasicAuthGuard)
     async createUser(@Body() dto: UserCreateModel): Promise<UserViewModel> {
